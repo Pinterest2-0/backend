b71446c80bb1fec6f486b88b065f0fcb98f5502e
Elijah <elijahoneal@yahoo.com>
2021-06-19 15:21:47 -0400

    Add CORS

--- api/server.js
+++ api/server.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const articleRouter = require('./articles/articles-router')
 const server = express()
 
 server.use(express.json())
+server.use(cors())
 
 server.use('/api/articles', articleRouter)
 

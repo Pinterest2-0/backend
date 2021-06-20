20d592b63c50093bcf7fdf958697fa3304b92683
Elijah <elijahoneal@yahoo.com>
2021-06-19 22:13:34 -0400

    Add CRUD endpoints

--- api/articles/articles-model.js
+++ api/articles/articles-model.js
@@ -1,9 +1,37 @@
 const db = require('../../data/db-config')
 
-const get = () => {
+const getAll = () => {
     return db('articles')
 }
 
+const getById = (id) => {
+    return db('articles').where({id}).first()
+}
+
+const getByCat = (category) => {
+    return db('articles').where({category}).first()
+}
+
+const insert = async (article) => {
+    const [id] = await db('articles').insert(article)
+    return getById(id)
+}
+
+const update = async (id , changes) => {
+    await db('articles').where({id}).update(changes)
+
+    return getById(id)
+}
+
+const remove = (id) => {
+    return db('articles').where({id}).del()
+}
+
 module.exports = {
-    get
+    getAll,
+    getById,
+    getByCat,
+    insert,
+    update,
+    remove
 }
--- api/articles/articles-router.js
+++ api/articles/articles-router.js
@@ -10,4 +10,41 @@ router.get('/', (req, res) => {
     } )
 })
 
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+    Article.getById(id)
+    .then( article => res.status(200).json(article) )
+    .catch( err => {
+        console.log(err)
+        res.status(500).json({ message: 'Could not get specific article' })
+    } )
+})
+
+router.post('/', (req, res) => {
+    Article.insert(req.body)
+    .then( newArticle => res.status(201).json(newArticle) )
+    .catch( err => {
+        console.log(err)
+        res.status(500).json({ message: 'Could not post new article' })
+    } )
+})
+
+router.put('/:id', (req, res) => {
+    Article.update(req.params.id , req.body)
+    .then( updatedArticle => res.status(200).json(updatedArticle) )
+    .catch( err => {
+        console.log(err)
+        res.status(500).json({ message: 'Could not update article' })
+    } )
+})
+
+router.delete('/:id', (req, res) => {
+    Article.delete(req.param.id)
+    .then( res.status(200) )
+    .catch( err => {
+        console.log(err)
+        res.status(500).json({ message: 'Could delete specific article' })
+    } )
+})
+
 module.exports = router;
